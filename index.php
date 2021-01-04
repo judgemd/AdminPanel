978400aa8946d5dce46485639d725a287efad938
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-04 13:23:11 +0300

    - Added Bootstrap and fonts - style.css edited - index.php edited

--- index.php
+++ index.php
@@ -6,10 +6,28 @@
 <body>
     <div class="all">
     <div class="leftbar">
-    
-    
+    <div class="dashboard">
+    <h3> Dashboard <h3>
     </div>
-    
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>    
     </div>
 </body>
 </html>
--- theme/headtags.php
+++ theme/headtags.php
@@ -4,10 +4,13 @@
 
 <!--Meta Tags-->
 <meta charset="utf8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 <!--Meta Tags-->
 
 <!--Styles-->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
 <!--Styles-->
 
 <!--Scripts-->
