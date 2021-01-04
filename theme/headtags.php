c0660c528ba460b437649d37d8d0bf09e7d5c2b1
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-04 11:14:41 +0300

    - Theme folder created - Headtags.php created and edited

---
+++ theme/headtags.php
@@ -0,0 +1,14 @@
+<head>
+
+<title>Your Website Name Here!</title>
+
+<!--Meta Tags-->
+<meta charset="utf8">
+
+<!--Styles-->
+<link rel="stylesheet" href="main.css">
+
+<!--Scripts-->
+<script src="main.js"></script>
+
+</head>
