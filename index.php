2cc8c6ca7e75f421c3f16b14535c5dac745ac5e3
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-04 17:29:55 +0300

    - Added "sidebarsitename" class inside index.php - Added "Log Out" and "Change User" options and configure in css

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 <body>
     <div class="all">
     <div class="leftbar">
-        <h1>Your Site Name Here!</h1>
+        <h1 class="sidebarsitename">Your Site Name Here!</h1>
     <div class="dashboard">
     <h3 class="active">Dashboard<h3>
     </div>
+
     <div class="users">
         <h3>Users<h3>
     </div>
@@ -24,14 +25,22 @@
         <br>
         <br>
         <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>    
+        <div class="altsidebar">
+        <div class="logout">
+        <h3 class="colormagenta">Log Out</h3>    
+        </div>
+        <div class="changeuser">
+        <h3 class="colormagenta">Change User</h3>
+        </div>
+        </div>
     </div>
+    <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
 </body>
 </html>
