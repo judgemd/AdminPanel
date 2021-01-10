f9c9e1a97410a3885519b110617b1eec881f145e
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-10 14:15:40 +0300

    I tried create a data via admin panel

--- createparagraph.php
+++ createparagraph.php
@@ -1,16 +1,34 @@
 <?php
+
+    $sayi = 1;
+
     include("system/connection.php");
 
    $cp = $_POST["createparagraph"];
 
-    $sql = "INSERT INTO mainphp (id, mainparagraph) VALUES (1, 'testparagraph')";
 
-    if(mysqli_query($conn, $sql)) {
-        echo "successfull";
-    } else {
+    
+        $sql = "INSERT INTO mainphp (id, mainparagraph) VALUES ($sayi, 'testparagraph')";
+    
+        if(mysqli_query($conn, $sql)) {
+            echo "successfull";
+        }    
+    
+    while(mysqli_error($conn)) {
+        $sayi++;
+        $sql = "INSERT INTO mainphp (id, mainparagraph) VALUES ($sayi, '$cp')";
+    
+        if(mysqli_query($conn, $sql)) {
+            break;
+            echo "successfull";
+        }    
+    }
+    
+    
+    /*else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
   mysqli_close($conn);
-  
+    */
 
 ?>
