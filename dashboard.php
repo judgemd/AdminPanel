af94c1f44be955c3e8101a4981ac07991c2eebc6
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-10 13:21:07 +0300

    Form system tested

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,5 @@
 <form action="changeparagraph.php" method="post">
 <label for="changeparagraph"><b>Change</b></label>
     <input type="text" placeholder="Change Paragraph" name="changeparagraph" required>
-    <button type="submit">Change Paragraph</button></form>
+    <button type="submit">Change Paragraph</button>
+    </form>
