4773d17d0fa3d341020b9e268d8315debef5f2c2
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-11 13:09:34 +0300

    I tried printing all data in the database

--- main.php
+++ main.php
@@ -7,12 +7,17 @@
     <body>
         
     <?php
-    
-    $sorgu = $conn -> query("SELECT * FROM mainphp WHERE id=5");
-    $outputs = $sorgu->fetch_array();
         
-         echo $output["mainparagraph"];
-        
+    $id = $conn -> query("SELECT * FROM mainphp ORDER BY id DESC LIMIT 1");
+    $outputs = $id->fetch_array();
+        $maxnumber = $outputs["id"];
+        $minnumber = 1;
+        while($minnumber <= $maxnumber){
+        $sorgu = $conn -> query("SELECT mainparagraph FROM mainphp WHERE id=$minnumber");    
+        $output = $sorgu->fetch_array();
+         echo $output["mainparagraph"] . "<br>";
+         $minnumber++;
+         }
         
         /*
     //////////////////////////////////////////
