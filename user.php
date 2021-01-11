f7ccc7fe9e10e50112a9ec8be1e0b7e78b70b218
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-11 13:27:43 +0300

    added "total number of data registered in the database" system

--- user.php
+++ user.php
@@ -1,1 +1,9 @@
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, illo voluptatem eveniet sed qui odit. Libero labore reiciendis illum porro suscipit aspernatur maiores laborum nisi at natus enim asperiores nemo voluptate accusamus sequi perferendis possimus veniam officiis sapiente est laudantium nulla, dolor similique? Ducimus distinctio quo recusandae, commodi autem ad earum eos tempore hic quaerat velit. Praesentium et, soluta doloribus dignissimos, rem delectus nulla optio numquam fuga ullam quod, earum dolores aliquam ab! Fuga hic, placeat sapiente et, necessitatibus earum laboriosam molestiae quas sunt voluptatem voluptates cupiditate asperiores tempora dicta quam corporis porro voluptatibus amet voluptatum doloribus. Modi, rerum tempora!
+Total number of data registered in the database
+<?php
+    include("system/connection.php");
+    $id = $conn -> query("SELECT * FROM mainphp ORDER BY id DESC LIMIT 1");
+    $outputs = $id->fetch_array();
+        echo $outputs["id"];
+
+
+?>
