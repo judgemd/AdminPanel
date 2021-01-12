b486244fd611594746f83702b014807d979566a3
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-12 15:39:30 +0300

    i tried make the code in main.php more readable

--- main.php
+++ main.php
@@ -1,9 +1,5 @@
-<?php
-    include("system/connection.php");
-?>
 <html>
-    <?php include("theme/headtags.php");?>
-
+<?php include("theme/headtags.php"); ?>
     <body>
         <table>
             <tr>
@@ -11,29 +7,22 @@
             </tr>
 
     <?php
-        
-    $id = $conn -> query("SELECT * FROM mainphp ORDER BY id DESC LIMIT 1");
-    $outputs = $id->fetch_array();
-        $maxnumber = $outputs["id"];
-        $minnumber = 1;
-        while($minnumber <= $maxnumber){
-        $sorgu = $conn -> query("SELECT mainparagraph FROM mainphp WHERE id=$minnumber");    
-        $output = $sorgu->fetch_array();
-         echo "<tr>" . "<th class='datas'>" . $output["mainparagraph"] . "</th>" . "</tr>";
-         $minnumber++;
-         }
-        
-        /*
-    //////////////////////////////////////////
+        include("system/connection.php");
 
-        $sqlquery = "UPDATE datas SET datas = 'notest' WHERE datas='test'";
-        if ($conn->query($sqlquery) === TRUE) {
-            echo "Succesfull UPDATE";
-        } 
-        else {
-            echo "ERROR: " . $conn->error;
-        }
-      */  
+        $id = $conn -> query("SELECT * FROM mainphp ORDER BY id DESC LIMIT 1");
+        $outputs = $id->fetch_array();
+        
+            $maxnumber = $outputs["id"];
+            $minnumber = 1;
+        
+        while($minnumber <= $maxnumber){
+            $sorgu = $conn -> query("SELECT mainparagraph FROM mainphp WHERE id=$minnumber");    
+            $output = $sorgu->fetch_array();
+         
+            echo "<tr>" . "<th class='datas'>" . $output["mainparagraph"] . "</th>" . "</tr>";
+         
+            $minnumber++;
+         }
  ?>
             </table>
     </body>
