95ffc4ab5ffe4d2ece8d6a8dd90a9f03f8743320
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-12 11:05:44 +0300

    added "Change Paragraph By ID" Form

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,22 @@
 <form action="changeparagraph.php" method="post">
-<label for="changeparagraph"><b>Change</b></label>
+<label for="changeparagraph"><b>Change Paragraph</b></label>
     <input type="text" placeholder="Change Paragraph" name="changeparagraph">
     <input type="text" placeholder="Paragraph to Change" name="paragraphtochange">
     <button type="submit">Change Paragraph</button>
 
     </form>
     <br>
+    <form action="changeparabyid" method="post">
+        <label for="changeparabyid"><b>Change Paragraph By ID</b></label>
+        <input type="text" placeholder="Change Paragraph" name="changeparagraph">
+        <input type="text" placeholder="ID" name="changeid">
+        <button type="submit">Change Paragraph By ID</button>
+    </form>
+
+    <br>    
 
     <form action="createparagraph.php" method="post">
     <label for="createparagraph"><b>Change</b></label>
     <input type="text" placeholder="Create Paragraph" name="createparagraph">
     <button type="submit">Create Paragraph</button>
     </form>
-
