d33a1039f885faf4ece4b4381c14bf2a3a57e544
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-12 10:26:16 +0300

    Edited "Change Paragraph" part

--- changeparagraph.php
+++ changeparagraph.php
@@ -2,8 +2,9 @@
     include("system/connection.php");
 
 $change = $_POST["changeparagraph"];
-     
-$sqlquery = "UPDATE mainphp SET mainparagraph = '$change' WHERE id='1'";
+$paragraphtochange = $_POST["paragraphtochange"];
+
+$sqlquery = "UPDATE mainphp SET mainparagraph = '$change' WHERE mainparagraph= '$paragraphtochange' ";
 if ($conn->query($sqlquery) === TRUE) {
     echo "Succesfull UPDATE";
 } 
--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
 <form action="changeparagraph.php" method="post">
 <label for="changeparagraph"><b>Change</b></label>
     <input type="text" placeholder="Change Paragraph" name="changeparagraph">
+    <input type="text" placeholder="Paragraph to Change" name="paragraphtochange">
     <button type="submit">Change Paragraph</button>
 
     </form>
