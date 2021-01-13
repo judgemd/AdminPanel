ccaeb87df640535baaf7c004ef05e07b4efe92a1
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-13 12:08:26 +0300

    Added "Delete Paragraph By ID" system

---
+++ deleteparagraphbyid.php
@@ -0,0 +1,21 @@
+<?php
+
+    include("system/connection.php");
+
+$delete = $_POST["deleteparagraphbyid"];
+
+$sqlquery = "DELETE FROM mainphp WHERE id = '$delete'";
+
+    if ($conn->query($sqlquery) === TRUE) {
+        echo "Succesfull DELETE";
+    } 
+    else {
+        echo "ERROR: " . $conn->error;
+    }
+
+
+
+
+
+
+?>
