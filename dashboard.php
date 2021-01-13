78762e8cd3959f967dbf452a2259972f0a83b9d4
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-13 11:19:05 +0300

    Unnecessary code has been deleted

--- dashboard.php
+++ dashboard.php
@@ -40,7 +40,6 @@
     <form action="deleteparagraph.php" method="post">
         <label for="deleteparagraph"><b>Delete</b></label>
         <input type="text" placeholder="Delete Paragraph" name="deleteparagraph">
-        <input type="text" placeholder="Paragraph to Delete" name="paragraphdelete">
         <button type="submit">Delete Paragraph</button>
     </form>
 
