53b302b2bad3c8b59c0572fce560d7b732e3f8c3
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-18 18:52:26 +0300

    moved head tags in main.php

--- main.php
+++ main.php
@@ -1,23 +1,13 @@
 <html>
-<?php
+   <!--Head Part-->
+   
+   <?php
     session_start();
-
-    ?>
-<head>
-
-<title>Your Website Name Here!</title>
-
-<!--Meta Tags-->
-<meta charset="utf8">
-<meta content="width=device-width, initial-scale=1.0" name="viewport" />
-<!--Meta Tags-->
-
-<!--Styles-->
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-<link rel="stylesheet" href="mainphp.css">
-<!--Styles-->
-</head>
-<?php include("theme/header.php")?>
-
+    include("theme/mainphphead.php");
+   ?>
+   <!--Head Part-->
+   
+   <!--Header-->
+   <?php include("theme/header.php")?>
+   <!--Header-->
 </html>
