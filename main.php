ba83bc8827d71b7cf15e576947e6058356727667
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-18 17:34:34 +0300

    added Home and Contact links and mainphp.css edited

--- main.php
+++ main.php
@@ -18,11 +18,14 @@
 <link rel="stylesheet" href="mainphp.css">
 <!--Styles-->
 </head>
-<header>
-    <h1>DevMDeniz</h1>
-    <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><i href="fa fa-moon-s"></i></li>
-</header>
+    <header>
+        <nav>
+            <h1>DevMDeniz</h1>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><i href="fa fa-moon-s"></i></li>
+              </ul>    
+        </nav>
+    </header>
 </html>
