87bc6f73630874d20aa67ed6e3ab01a805c7f362
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-24 22:05:38 +0300

    some bugs fixed

--- theme/index/headtags.php
+++ theme/index/headtags.php
@@ -13,7 +13,7 @@
 
 <!--Styles-->
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/index.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
 <!--Styles-->
 
