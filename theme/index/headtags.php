ee67a1d35d39890ec98e794b76677781f47f005c
judgemd <esref.mamedov.2018@gmail.com>
2021-02-02 00:08:36 +0400

    Include bootstrap and font awesome technologies

--- theme/index/headtags.php
+++ theme/index/headtags.php
@@ -14,7 +14,8 @@
 <!--Styles-->
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 <link rel="stylesheet" href="css/index.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!--Styles-->
 
 <!--Scripts-->
